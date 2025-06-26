4fbf3d86564782c35b3151efb25475070e119308
HoangNg <hoangnguyen@hoppergames.io>
2025-06-26 13:52:10 +0700

    Backup code

---
+++ Assets/_game/Scripts/Gameplay/Wave/EnemyWave.cs
@@ -0,0 +1,28 @@
+// using UnityEngine;
+//
+// /// <summary>
+// /// Wave
+// /// monster Count
+// /// gapTime: time duration between two period of times to spawn Enemy;
+// ///
+// /// Start => interval spawn enemy after each gapTime;
+// ///
+// /// Spawn => EntityManager.Spawn(this, data)
+// /// </summary>
+//
+// public class EnemyWaveData
+// {
+//     public int enemyId;  //=> Load EnemyConfig(enemyId)
+//     public float interval;
+//     
+// }
+//
+// public interface IEnemyWave
+// {
+//     public void Start();
+// }
+//
+// public class EnemyWave : IEnemyWave
+// {
+//     
+// }
